71e19e5cfc01b8aa03559b547953611d9beacc14
Abdul Karim <abdulkarimmandvia76@gmail.com>
2024-03-11 04:30:27 -0700

    20% done

---
+++ main.mjs
@@ -0,0 +1,25 @@
+// Problem 1 \\
+let city = prompt("Enter City Name");
+if (city == "karachi"){
+    console.log(`Welcome to city of lights`)
+}
+
+// Problem 2 \\
+let gender = prompt(("Enter Your Gender:"));
+
+if(gender == "male"){
+    console.log(`Good Morning Sir`);
+}else {
+    console.log(`Good Morning Ma'am`);
+}
+
+let signalColor = prompt("Enter Color of road traffic signal")
+
+if (signalColor === "red"){
+    console.log(`Must Stop`)
+}
+else if (signalColor === "yellow"){
+    console.log(`Ready To Move`)
+}else{
+    console.log(`Move Now`)
+}
